4e17971045553708b9db7c8cf0baaafc428828b9
CaszGamerMD <caszarted@gmail.com>
2018-07-20 05:47:23 -0500

    add idea list- can be moved

--- src/main/java/me/caszgamermd/noottag/NootTag.java
+++ src/main/java/me/caszgamermd/noottag/NootTag.java
@@ -13,3 +13,25 @@ public class NootTag extends JavaPlugin {
     }
 
 }
+////notes: move to a better location ***
+////
+//// "lobby" place to select tag arena / must have a "Tag Ticket" to play? -- used to control Win farming
+//// game launch cooldown like must wait 15 mins after game completes before next one starts..?
+//// arena where players cant build/destroy, stash current inv, lock all but hotbar.
+////
+//// scoreboard
+//// loot table?* -- otherwise PhatNoot/give dependent
+//// loot table* based on # of players
+//// potion effect on locations - can be set with a "tool"?
+//// *if tie, all "winners" get lowest earned prize... (example: 11 players, 5 man tie, all 5 would get a 3rd place prize.)
+//// multi loot idea*: if P[2-4], 1 winner; P[5-10], 2 winner; P[11-15], 3 winner;
+//// time scales in tiers with prizes, 5,8,12 min games
+//// 12 min dame has dmg on, other 2 have dmg off
+////
+////GUI to set loot table*
+////potions that can fall and %
+////set player/time limits
+////
+////tagged player has a incresing timer, potion effect
+////it player has immunty from being tagged back, after passing "it" for 2-4 secs / and a small speed boost?
+////allow customization of as much as possible as easily as possible for servers.
