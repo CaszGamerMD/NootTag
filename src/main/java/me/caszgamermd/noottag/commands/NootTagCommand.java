34c761cf970e02e61c5e0c8b77ceb6ac4e6c2f4c
Haileykins <hailey.loralyn@gmail.com>
2018-07-21 10:32:38 -0700

    Handle Join/Leave PlayerStates

--- src/main/java/me/caszgamermd/noottag/commands/NootTagCommand.java
+++ src/main/java/me/caszgamermd/noottag/commands/NootTagCommand.java
@@ -1,5 +1,7 @@
 package me.caszgamermd.noottag.commands;
 
+import me.caszgamermd.noottag.utils.GameState;
+import me.caszgamermd.noottag.utils.PlayerState;
 import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.command.Command;
@@ -33,13 +35,32 @@ public class NootTagCommand implements CommandExecutor {
 
                 if (args[0].equalsIgnoreCase("join")) {
                     player.teleport(lobby); //
+                    if (PlayerState.isState(PlayerState.INLOBBY, player)) {
+                        player.sendMessage("Your Already In A Lobby"); //
+                        return true;
+                    }
+
+                    if (PlayerState.isState(PlayerState.INGAME, player)) {
+                        player.sendMessage("Your Already In A Game!"); //
+                        return true;
+                    }
+
                     player.sendMessage("You were teleported to the LOBBY!"); //
                     return true;
+
                 }
 
                 if (args[0].equalsIgnoreCase("leave")) {
                     player.teleport(spawn); //
-                    player.sendMessage("You Left The LOBBY!"); //
+                    if (PlayerState.isState(PlayerState.INLOBBY, player)) {
+                        player.sendMessage("You Left The LOBBY!"); //
+                        return true;
+                    }
+
+                    if (PlayerState.isState(PlayerState.INGAME, player)) {
+                        player.sendMessage("You Left The GAME!"); //
+                        return true;
+                    }
                     return true;
                 }
                 
