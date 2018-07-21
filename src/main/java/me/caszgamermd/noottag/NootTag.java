0e091a67e5317e44ce79e72368346b53a5c0f8f9
Haileykins <hailey.loralyn@gmail.com>
2018-07-21 03:36:55 -0700

    Create Player Based Commands

--- src/main/java/me/caszgamermd/noottag/NootTag.java
+++ src/main/java/me/caszgamermd/noottag/NootTag.java
@@ -1,6 +1,7 @@
 package me.caszgamermd.noottag;
 
 import me.caszgamermd.noottag.arena.ArenaManager;
+import me.caszgamermd.noottag.commands.NootTagCommand;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class NootTag extends JavaPlugin {
@@ -8,6 +9,8 @@ public class NootTag extends JavaPlugin {
     @Override
     public void onEnable() {
         ArenaManager arenaManager = new ArenaManager();
+
+        getCommand("noottag").setExecutor(new NootTagCommand());
     }
 
     @Override
---
+++ src/main/java/me/caszgamermd/noottag/commands/NootTagCommand.java
@@ -0,0 +1,53 @@
+package me.caszgamermd.noottag.commands;
+
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class NootTagCommand implements CommandExecutor {
+
+    // Inline Comments (//) Represent Pseudocode Lines
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (sender instanceof Player) {
+
+            Player player = (Player) sender;
+
+            World world = player.getWorld(); //
+            Location lobby = new Location(world, -15019, 85, 7385); //
+            Location spawn = new Location(world, -15003, 78, 7434); //
+
+            if (player.getWorld().getName().equalsIgnoreCase("world")) {
+
+                System.out.println("World world confirmed");
+
+                if (args.length == 0) {
+                    // TODO HELP MENU?
+                    System.out.println("Help Command Received"); //
+                    return true;
+                }
+
+                if (args[0].equalsIgnoreCase("join")) {
+                    player.teleport(lobby); //
+                    player.sendMessage("You were teleported to the LOBBY!"); //
+                    return true;
+                }
+
+                if (args[0].equalsIgnoreCase("leave")) {
+                    player.teleport(spawn); //
+                    player.sendMessage("You Left The LOBBY!"); //
+                    return true;
+                }
+
+                return true;
+            }
+            player.sendMessage("Unknown Command");
+            return true;
+        }
+        return true;
+    }
+}
