2d9e99279198ebea0dfc74087254d4cb1ea8ef7c
Steven Hormell <stevenahormell@gmail.com>
2018-07-21 00:54:08 -0700

    Added Optional getters

--- src/main/java/me/caszgamermd/noottag/arena/ArenaManager.java
+++ src/main/java/me/caszgamermd/noottag/arena/ArenaManager.java
@@ -2,6 +2,7 @@ package me.caszgamermd.noottag.arena;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 import java.util.UUID;
 
 public class ArenaManager {
@@ -18,16 +19,19 @@ public class ArenaManager {
         // LOADING ARENAS FROM CONFIG
     }
 
-    public Arena getArena(String arenaName) {
-        return (Arena) arenas.entrySet().stream().filter(entry -> entry.getValue().getArenaName().equalsIgnoreCase(arenaName)).findFirst().orElse(null);
+    public Optional<Arena> getArena(String arenaName) {
+        Map.Entry<Integer, Arena> map = arenas.entrySet().stream().filter(entry -> entry.getValue().getArenaName().equalsIgnoreCase(arenaName)).findFirst().orElse(null);
+        return Optional.ofNullable(map.getValue());
     }
 
-    public Arena getArena(int arenaID) {
-        return (Arena) arenas.entrySet().stream().filter(entry -> entry.getValue().getId() == arenaID).findFirst().orElse(null);
+    public Optional<Arena> getArena(int arenaID) {
+        Map.Entry<Integer, Arena> map = arenas.entrySet().stream().filter(entry -> entry.getValue().getId() == arenaID).findFirst().orElse(null);
+        return Optional.ofNullable(map.getValue());
     }
 
-    public Arena getArena(UUID uuid) {
-        return (Arena) arenas.entrySet().stream().filter(entry -> entry.getValue().getPlayers().containsKey(uuid)).findFirst().orElse(null);
+    public Optional<Arena> getArena(UUID uuid) {
+        Map.Entry<Integer, Arena> map = arenas.entrySet().stream().filter(entry -> entry.getValue().getPlayers().containsKey(uuid)).findFirst().orElse(null);
+        return Optional.ofNullable(map.getValue());
     }
 
 
